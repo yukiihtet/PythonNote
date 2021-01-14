0f4e53b24a1df7191343a403596895c124d310ab
yukiihtet <seinthanhtet@gmail.com>
2021-01-14 12:44:47 +0630

    Data Type Lab

---
+++ DataTypeLab.py
@@ -0,0 +1,41 @@
+"String Data Tpye"
+x="Htet"
+print(x)
+print(type(x))
+
+"Numeric Data Type"
+y=99
+print(y)
+print(type(y))
+
+x=9.9
+print(x)
+print(type(x))
+
+"Sequence Data Type"
+mylist=[50,"green",x]
+print(mylist)
+print(type(mylist))
+
+myTuple=(50,'green',x)
+print(myTuple)
+print(type(myTuple))
+
+myDict={'name':"Htet",'age':20}
+print(myDict)
+print(type(myDict))
+
+"Set  Data Type"
+mySet={'a','c','b',x,'a','a'}
+print(mySet)
+print(type(mySet))
+
+"Boolean Data Type"
+myBool=True
+print(myBool)
+print(type(myBool))
+
+"Byte Data Type"
+myByte=b"Htet Htet"
+print(myByte)
+print(type(myByte))
