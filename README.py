a2869858fd89f4a9e27302a31793661c92b112dc
yukiihtet <seinthanhtet@gmail.com>
2021-01-26 19:59:25 +0630

    added README.py

---
+++ README.py
@@ -0,0 +1,20 @@
+Python-Programming
+Python Training for Pure Programming
+Notes
+No.	Title
+0x00	ASCII Table
+Course outline
+No.	Title
+0x00	Introduction
+0x01	Installation & Setups
+0x02	Hello World Program
+0x03	Data Types
+0x04	Strings
+0x05	Operators
+0x06	Numeric
+0x07	Conditions
+0x08	Loops
+0x09	Lists & Tuple
+0x0a	Set & Dictionary
+0x0b	Functions
+0x0c	String Formatting
